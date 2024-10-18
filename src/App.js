123e37170595065bf8cf92a5c12199a61bdecf1c
Tynchtyk <inshallatynchtyk@gmail.com>
2024-10-18 10:28:07 +0600

    add: spa page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+ 
+ import Demo from "./Demo";
+ import Button from "./ui/Button";
 
+import { useState } from "react";
+// props -eto  csposob peredaet dannyx Js ot reditelskogo kompanenta k dochernom komponentam
+// children eto sboistvo parnovo elemente 
+/// 
 function App() {
+  const [toggle, setToggle] = useState(false)
+  const [deled, setDeled] = useState(false)
+  const [add, setAdd] = useState(false)
+  const [edit, setEdit] = useState(false)
+
+
+const toggleHandler = () => {
+  setToggle(prevState => !prevState)
+}
+const deletedHandler = () => {
+  setDeled(prevState => !prevState)
+}
+   
+const addHandler = () => {
+  setAdd(prevState => !prevState)
+
+}
+const editHandler = () => {
+  setEdit(prevState => !prevState)
+
+}
+   
+ let nums = [1, 2, 3, 4, 5, 6]
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      {edit && <section style={{fontSize: '3rem', width: '300px',height: '300px', backgroundColor: 'red', alignItems: "center"}}> MONACO</section>}
+      {toggle && <p>Hello</p>}
+      {deled && <h1>Tompochka</h1>}
+      {add && <h2 style={{color: 'red'}}>SOme add page</h2>}
+ Hello
+ <Demo text={'This is a demo page'} list={nums}/>
+<Button onClick={toggleHandler}>Toggle</Button>
+ <Button onClick={deletedHandler}>Delete</Button>
+ <Button onClick={addHandler}>Add</Button>
+ <Button onClick={editHandler}>Edit</Button>
     </div>
   );
 }
---
+++ src/Demo.jsx
@@ -0,0 +1,15 @@
+const Demo = (props) => {
+    console.log(props);
+    let sortedNums = props.list.sort((a, b) => a - b)
+    return  (
+        <div>
+ 
+<h1>Demo Page</h1>
+        <p>{props.text}</p>
+<p>{sortedNums}</p>
+        </div>
+   
+    )
+
+}
+export default Demo
